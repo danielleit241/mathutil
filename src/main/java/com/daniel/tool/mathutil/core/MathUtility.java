d44e165cfa1a4ae63595b1f4b51b4bffff69dfe2
danielleit241 <danielleee241@gmail.com>
2025-06-12 17:11:13 +0700

    update main code to see green

--- src/main/java/com/daniel/tool/mathutil/core/MathUtility.java
+++ src/main/java/com/daniel/tool/mathutil/core/MathUtility.java
@@ -29,8 +29,12 @@ public class MathUtility {
 //        return result;
 //    }
     public static long getFactorial(int n){
+        if(n < 0 || n > 20)
+            throw new IllegalArgumentException("n must be between 0 and 20");
+
         if(n == 0 || n == 1)
             return 1;
+
         return n*getFactorial(n-1);
         //ĐỆ QUY - RECURSION - GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
     }
